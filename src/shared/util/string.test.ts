bb8cfa3a9323f95d0a7ee5addd34aa8037d125e8
shivankar <shivankar.s@greyorange.com>
2024-08-29 11:53:05 +0530

    Add Function With All The Test Cases

--- src/shared/util/string.test.ts
+++ src/shared/util/string.test.ts
@@ -1,11 +1,106 @@
 import { add } from "./string";
 
 describe("String Adder", () => {
-  it.todo("Handle Empty Strings");
-  it.todo("Handle Single Digit Strings");
-  it.todo("Handle String Separated By Delimeter");
-  it.todo("Handle String Separated By Delimeter With Different Delimeter");
-  it.todo("Handle Only Delimeter");
-  it.todo("Handle String Starting With Delimeter");
-  it.todo("Handle String Starting With Delimeter");
+  it("Handle Empty Strings", () => {
+    const numbers = "";
+    const result = add(numbers);
+
+    expect(result).toBe(0);
+  });
+
+  it("Handle Single Digit Strings", () => {
+    const numbers = "1";
+    const result = add(numbers);
+
+    expect(result).toBe(1);
+  });
+
+  it("Handle String Separated By Delimeter", () => {
+    const numbers = "1,2";
+    const result = add(numbers);
+
+    expect(result).toBe(3);
+  });
+
+  it("Handle New Line Separated String With Delimeters", () => {
+    const numbers = "1\n2,3";
+    const result = add(numbers);
+
+    expect(result).toBe(6);
+  });
+
+  it("Handle String Separated By Delimeter With Different Delimeter", () => {
+    const numbers = "//;\n1;2";
+    const result = add(numbers);
+
+    expect(result).toBe(3);
+  });
+
+  it("Handle Only Delimeter", () => {
+    const numbers = ",";
+    const result = add(numbers);
+
+    expect(result).toBe(0);
+  });
+
+  it("Handle Only Custom Delimeter", () => {
+    const numbers = "//;\n";
+    const result = add(numbers);
+
+    expect(result).toBe(0);
+  });
+  it("Handle String Starting With Delimeter", () => {
+    const numbers = ",1,2";
+    const result = add(numbers);
+
+    expect(result).toBe(3);
+  });
+
+  it("Handle String Ending With Delimeter", () => {
+    const numbers = "1,2,";
+    const result = add(numbers);
+
+    expect(result).toBe(3);
+  });
+
+  it("Handle String with String instead of Numbers", () => {
+    const numbers = "1,a";
+    const result = add(numbers);
+
+    expect(result).toBe(1);
+  });
+
+  it("Handle String with Negative Numbers", () => {
+    try {
+      const numbers = "1,-2";
+      add(numbers);
+    } catch (e) {
+      if (e instanceof Error)
+        expect(e.message).toBe("negative numbers not allowed -2");
+    }
+  });
+
+  it("Handle String with Multiple Negative Numbers", () => {
+    try {
+      const numbers = "1,-2,-3";
+      add(numbers);
+    } catch (e) {
+      if (e instanceof Error)
+        expect(e.message).toBe("negative numbers not allowed -2,-3");
+    }
+  });
+
+  it("Handle Multiple Delimteres", () => {
+    const numbers = "1,2;3";
+    const result = add(numbers);
+
+    expect(result).toBe(3);
+  });
+
+  it("Handle Multiple Delimteres With Custom Delimeter", () => {
+    const numbers = "//;\n1;2,3";
+    const result = add(numbers);
+
+    expect(result).toBe(3);
+  });
 });
--- src/shared/util/string.ts
+++ src/shared/util/string.ts
@@ -1,3 +1,61 @@
 export const add = (numbers: string): number => {
-  return 0;
+  // handle empty, null and undefined string
+  if (numbers === "" || numbers == null) return 0;
+
+  const defaultDelimeter = ",";
+  let delimeter = defaultDelimeter;
+
+  // Handle if the string is just the delimeter
+  if (numbers === defaultDelimeter) return 0;
+
+  try {
+    // need to check if the string starts with `//` and has a delimeter
+    if (numbers.startsWith("//")) {
+      // get the delimeter
+      delimeter = numbers[2];
+      // remove the delimeter from the string
+      numbers = numbers.slice(4);
+    }
+
+    //handle just the custom delimeter
+    if (!numbers) return 0;
+
+    // if the string has a new line character then replace it with the delimeter
+    if (numbers.includes("\n")) {
+      numbers = numbers.replace("\n", delimeter);
+    }
+
+    // splitting the string with the delimeter
+    const numbersArray = numbers.split(delimeter);
+
+    // check if the string has negative numbers
+    const negativeNumbers = numbersArray.filter((a) => parseInt(a) < 0);
+
+    // if the string has negative numbers then throw an error
+    if (negativeNumbers.length > 0) {
+      throw new Error(
+        `negative numbers not allowed ${negativeNumbers.join(",")}`
+      );
+    }
+
+    // sum all the numbers in the string
+    const answer = numbersArray.reduce((partialSum, a) => {
+      // check if the string has a number
+      if (isNaN(parseInt(a))) {
+        return partialSum;
+      }
+
+      return partialSum + parseInt(a);
+    }, 0);
+
+    return answer;
+  } catch (error) {
+    // handle unknown error
+    let message = "Unknown Error";
+
+    // check if the error is an instance of Error
+    if (error instanceof Error) message = error.message;
+
+    throw new Error(message);
+  }
 };
