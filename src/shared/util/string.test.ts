a012403cbd2c48018b06ed0af0cc3eea67d3f767
shivankar <shivankar.s@greyorange.com>
2024-08-29 10:50:48 +0530

    Added Test Cases

---
+++ src/shared/util/string.test.ts
@@ -0,0 +1,11 @@
+import { add } from "./string";
+
+describe("String Adder", () => {
+  it.todo("Handle Empty Strings");
+  it.todo("Handle Single Digit Strings");
+  it.todo("Handle String Separated By Delimeter");
+  it.todo("Handle String Separated By Delimeter With Different Delimeter");
+  it.todo("Handle Only Delimeter");
+  it.todo("Handle String Starting With Delimeter");
+  it.todo("Handle String Starting With Delimeter");
+});
---
+++ src/shared/util/string.ts
@@ -0,0 +1,3 @@
+export const add = (numbers: string): number => {
+  return 0;
+};
