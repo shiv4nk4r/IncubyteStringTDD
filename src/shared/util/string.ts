c1967444e74a55c641dbf73e808215f6f7636feb
shivankar <shivankar.s@greyorange.com>
2024-08-29 12:32:09 +0530

    Utility Function to get the numbers

--- src/shared/util/string.ts
+++ src/shared/util/string.ts
@@ -1,3 +1,10 @@
+/**
+ * Calculates the sum of numbers in a string.
+ *
+ * @param numbers - The string containing numbers to be summed.
+ * @returns The sum of the numbers in the string.
+ * @throws {Error} If the string contains negative numbers.
+ */
 export const add = (numbers: string): number => {
   // handle empty, null and undefined string
   if (numbers === "" || numbers == null) return 0;
@@ -59,5 +66,59 @@ export const add = (numbers: string): number => {
     if (error instanceof Error) message = error.message;
 
     throw new Error(message);
+  }
+};
+
+/**
+ * Fetches numbers from a string and returns an array of numbers.
+ *
+ * @param numbers - The string containing numbers.
+ * @returns An array of numbers extracted from the string.
+ */
+export const fetchNumbers = (numbers: string): number[] => {
+  // handle empty, null and undefined string
+  if (numbers === "" || numbers == null) return [];
+
+  const defaultDelimeter = ",";
+  let delimeter = defaultDelimeter;
+
+  // Handle if the string is just the delimeter
+  if (numbers === defaultDelimeter) return [];
+
+  try {
+    // need to check if the string starts with `//[delimeter]` and has a delimeter
+    if (numbers.startsWith("//[")) {
+      // find the end of the delimiter
+      const delimiterEndIndex = numbers.indexOf("]\n");
+      // get the delimiter
+      delimeter = numbers.substring(3, delimiterEndIndex);
+      // remove the delimiter from the string
+      numbers = numbers.slice(delimiterEndIndex + 2);
+    }
+
+    //handle just the custom delimeter
+    if (!numbers) return [];
+
+    // if the string has a new line character then replace it with the delimeter
+    if (numbers.includes("\n")) {
+      numbers = numbers.replace("\n", delimeter);
+    }
+
+    // splitting the string with the delimeter
+    const numbersArray = numbers.split(delimeter);
+
+    // sum all the numbers in the string
+    const answer = numbersArray.map((a) => {
+      // check if the string has a number
+      if (isNaN(parseInt(a))) {
+        return 0;
+      }
+
+      return parseInt(a);
+    }, 0);
+
+    return answer;
+  } catch (error) {
+    return [];
   }
 };
